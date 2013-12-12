08aad5cb8ca395180b3ed02800615313ea82650b
viktorus <viktort@bigmir.net>
2013-12-12 12:47:47 +0100

    added gotoLoginPage method

--- Tutorial/src/pages/HomePage.java
+++ Tutorial/src/pages/HomePage.java
@@ -10,11 +10,23 @@ import base.Page;
 public class HomePage extends Page {
 
 	@FindBy(linkText="Weather") private WebElement weatherLnk;
+	@FindBy(id="idcta-username") private WebElement signInLnk;
+	
 	
 	public HomePage() {
 		PageFactory.initElements(driver, this);
 	}
 		
+	/**
+	 * Open Weather Page
+	 * @return new WeatherPage
+	 */
+	public LoginPage gotoLoginPage() {
+		signInLnk.click();
+		Assert.assertEquals(driver.getTitle(), "BBC - Sign in", "Login page failed to open");
+		return new LoginPage(); 
+	}
+	
 	/**
 	 * Open Weather Page
 	 * @return new WeatherPage
